3ca24d0680145b6204cdc1508b6f54f2fb3c45d5
Woodstock <victorhe97@gmail.com>
2021-12-18 17:27:15 -0500

    Prompt works with the API key saved in .env file. Able to query 5 books

---
+++ README.md
@@ -0,0 +1,22 @@
+# Welcome to google-books-cli
+
+This is a command line application that will allow the user to query for 5 books using the Google Books API and save/remove them to a personal bookshelf.
+
+### Getting started
+
+These instructions assume that you have already downloaded Git and Node. These are some specific versions I am using:
+
+npm 8.1.0
+node v17.0.1
+
+1. To clone this repo, simply open up your terminal and type in: `git clone git@github.com:VictorHeDev/google-books-cli.git` (or copy and paste)
+2. Use your CLI and `cd` into the new directory that was downloaded. For example: `cd google-books-cli`
+3. Install the dependencies by running `npm install` or `npm i`
+
+### Dependencies used:
+Axios: used to simplify HTTP requests and parse JSON
+
+
+Testing:
+Jest?
+Mocha?
--- src/index.js
+++ src/index.js
@@ -1,2 +1,35 @@
-import chalk from 'chalk';
+require('dotenv').config();
+const fetch = require('node-fetch');
+// const readline = require('readline');
+// const rl = readline.createInterface(process.stdin, process.stdout);
+const prompt = require('prompt');
 
+prompt.start();
+
+prompt.get(['title'], async (err, result) => {
+  console.log('CLI input received: ');
+  console.log('title:' + result.title);
+
+  const response = await fetch(
+    `https://www.googleapis.com/books/v1/volumes?q=${result.title}&key=${process.env.API_KEY}`
+  );
+
+  const books = await response.json();
+  const items = books.items;
+
+  for (let i = 0; i < 5; i++) {
+    console.log(items[i].volumeInfo.title);
+  }
+
+  // console.log(items);
+});
+
+// rl.question('What book title do you want to search for?', async (res) => {
+//   const response = await fetch(
+//     `https://www.googleapis.com/books/v1/volumes?q=pride+prejudice&download=epub&key=${process.env.API_KEY}`
+//   );
+//   // console.log(process.env);
+//   console.log(await response.json());
+//   // console.log(`Your name is: ${res}`);
+//   rl.close();
+// });
